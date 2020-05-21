0a50184521a118947bdab6d52918a76d2e347c01
dev_mahmoud_ashraf <ashraaf.mahmoud@gmail.com>
2020-05-21 21:37:34 +0200

    add mockito stub test

---
+++ app/src/test/java/com/example/tddapp/mockito/stubtest/MockitoStubTest.java
@@ -0,0 +1,44 @@
+package com.example.tddapp.mockito.stubtest;
+
+import com.example.tddapp.Person;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
+
+public class MockitoStubTest {
+    // we want to simulate return value from mocking object instead default values
+    @Mock
+    Person person;
+
+    // fourth way to mock object by rule
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
+
+    @Test
+    public void testPersonIsNotNull(){
+        assertNotNull(person);
+    }
+
+    @Test
+    public void testPersonFullNameReturnApi(){
+        System.out.println(person.getAge());
+        System.out.println(person.getFullName());
+        when(person.getAge()).thenReturn(27);
+        when(person.getFullName()).thenReturn("mahmoud");
+        System.out.println(person.getAge());
+        System.out.println(person.getFullName());
+    }
+
+
+    @Test
+    public void testPersonFullNameThrowException(){
+        when(person.getFullName()).thenThrow(new IllegalStateException("Test then throw exception."));
+    }
+
+}
